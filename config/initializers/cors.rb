1a02d1c25cea821cc5618f4c05dd53cb5d05c3a1
大塚健登 <kent0tsuka1118@gmail.com>
2022-06-25 00:58:52 +0900

    Add：CORSの実装

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,11 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+		# 開発環境では以下の通りでOKだが、本番環境では実際のURLに書き換える
+		# localhost:8080：Vue.jsの開発環境
+    origins 'localhost:8080'
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
