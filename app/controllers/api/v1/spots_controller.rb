91f7430046e306a36a8fd3bb0e769215424e2822
大塚健登 <kent0tsuka1118@gmail.com>
2022-06-30 16:27:31 +0900

    Add：Spotテーブルのすべての地点とその国、動画を返すapiを作成 #6

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -18,13 +18,16 @@ class Api::V1::SpotsController < ApiController
     render json: { status: 'ok' }
   end
 
-  # Spotテーブルのすべての地点とその国を返す
+  # Spotテーブルのすべての地点とその国、動画を返す
   def all_spot
     spots = Spot.all.order(click_count: :desc)
     areas = []
+    videos = []
     spots.each do |spot|
       areas << spot.country
+      ramdam_data = Video.where(spot: spot.name).order("RANDOM()").first
+      videos << { video_id: ramdam_data.video_id, title: ramdam_data.title, thumbnail: ramdam_data.thumbnail, view_count: ramdam_data.view_count.to_s(:delimited), published_at: ramdam_data.published_at.strftime("%Y/%m/%d") }
     end
-    render json: { spots: spots, areas: areas }
+    render json: { spots: spots, areas: areas, videos: videos }
   end
 end
