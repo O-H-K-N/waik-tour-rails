45924cee3fa803b893e0e3f9669f02c33bbcd3cf
大塚健登 <kent0tsuka1118@gmail.com>
2022-07-08 16:40:51 +0900

    Fix：Herokuデプロイ時のアセットパイプラインの不具合を修正_3 #19

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,7 +1,7 @@
 //= require jquery3
-//= require rails-ujs
-//= require activestorage
 //= require popper
 //= require bootstrap-sprockets
-//= require edit_comment
-//= require preview
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
