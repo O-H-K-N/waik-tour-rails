180e725885a2196c49f7248230a89bc68dbeb087
大塚健登 <kent0tsuka1118@gmail.com>
2022-07-12 22:46:49 +0900

    Fix：Userのカラムパスワードのバリデーションを変更 #9

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
   validates :password, presence: true, on: :create
-  validates :password, length: { in: 8..20 }, if: -> { new_record? || changes[:crypted_password] }, allow_blank: true
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || changes[:crypted_password] }, allow_blank: true
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
