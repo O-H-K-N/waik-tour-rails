c41fff136f543a19e122ab921261a997bdfa9d91
大塚健登 <kent0tsuka1118@gmail.com>
2022-07-21 16:10:45 +0900

    Fix：コントローラ内で冗長性を含む箇所を解消 #25 #29

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SpotsController < ApiController
   # 国に紐づくスポット一覧を返す
   def index
     if params[:flag] == 'all_spot'
-      # Spotテーブルのすべての地点とその国、動画を返す
+      # Spotテーブルのすべての地点とその国、動画を地点クリック数順で返す
       all_spots = Spot.all.order(click_count: :desc)
       spots = []
       areas = []
@@ -24,36 +24,29 @@ class Api::V1::SpotsController < ApiController
       end
       render json: { spots: spots, areas: areas, videos: videos }
     elsif params[:flag] == 'new_spot'
-      # Spotテーブルのすべての地点とその国、動画を返す
+      # Spotテーブルのすべての地点とその国、動画を地点作成順で返す
       new_spots = Spot.all.order(created_at: :desc)
       spots = []
       areas = []
       videos = []
       new_spots.each do |spot|
         # 3日前までに作成された地点&videoを保有している地点のみ有効
-        if spot.recently? && Video.where(spot: spot.name) != []
-          spots << spot
-          areas << spot.country
-          ramdam_data = Video.where(spot: spot.name).order("RANDOM()").first
-          videos << {
-            video_id: ramdam_data.video_id,
-            title: ramdam_data.title,
-            thumbnail: ramdam_data.thumbnail,
-            view_count: ramdam_data.view_count.to_s(:delimited),
-            published_at: ramdam_data.published_at.strftime("%Y/%m/%d")
-          }
+        if spot.recently?
+          if Video.where(spot: spot.name) != []
+            spots << spot
+            areas << spot.country
+            ramdam_data = Video.where(spot: spot.name).order("RANDOM()").first
+            videos << {
+              video_id: ramdam_data.video_id,
+              title: ramdam_data.title,
+              thumbnail: ramdam_data.thumbnail,
+              view_count: ramdam_data.view_count.to_s(:delimited),
+              published_at: ramdam_data.published_at.strftime("%Y/%m/%d")
+            }
+          end
         end
       end
       render json: { spots: spots, areas: areas, videos: videos }
-    else
-      # 取得した国IDが有する地点情報を返す
-      country = Country.find(params[:country_id])
-      spots = country.spots
-      if spots.present?
-        render json: { area: country, spots: spots }
-      else
-        render json: country, status: :bad_request
-      end
     end
   end
 
