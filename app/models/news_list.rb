fdaf9870a938b4085f8301b217860e9e43065f98
大塚健登 <kent0tsuka1118@gmail.com>
2022-08-09 20:53:31 +0900

    Add：お知らせ情報のカラムに内容の英訳を追加 #15

--- app/models/news_list.rb
+++ app/models/news_list.rb
@@ -1,4 +1,6 @@
 class NewsList < ApplicationRecord
   validates :content, presence: true
+  validates :content_ens, presence: true
   validates :content, length: { in: 5..45 }, allow_blank: true
+  validates :content_ens, length: { in: 5..70 }, allow_blank: true
 end
