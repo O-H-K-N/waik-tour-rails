d0961b700a3c5c9700f8b6dfffe817b476a2207d
Kento Ohtsuka <81758321+O-H-K-N@users.noreply.github.com>
2022-12-17 16:53:38 +0900

    Cors修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'vtourhub.jp'
+    origins 'https://vtourhub.jp/'
 
     resource '*',
       headers: :any,
