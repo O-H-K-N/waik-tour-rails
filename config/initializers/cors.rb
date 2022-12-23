304ff98e7dd97c1acff0a42eec5fdff62716f2d0
大塚健登 <kent0tsuka1118@gmail.com>
2022-12-24 00:25:40 +0900

    Add：本番用管理者ユーザを作成

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://vtourhub.jp'
+    origins 'vtourhub.jp'
 
     resource '*',
       headers: :any,
