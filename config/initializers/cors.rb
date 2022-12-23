b112fa747eb8bf8246dd3fa3cc786a1b00ff1dfb
大塚健登 <kent0tsuka1118@gmail.com>
2022-12-24 00:43:12 +0900

    Fix：コンフリクト解消

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+<<<<<<< HEAD
     origins '*'
+=======
+    origins 'vtourhub.jp'
+>>>>>>> 304ff98 (Add：本番用管理者ユーザを作成)
 
     resource '*',
       headers: :any,
